7072a5f1fcf7a3996974647790b67fda9d1d464f
Javier Athallah <jeviganteng53@gmail.com>
2025-07-28 17:35:41 +0700

    Fix Vercel deployment: Update API structure and static file serving

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,30 @@
-import { createServer } from '../server/index.js';
+import "dotenv/config";
+import express, { type Request, Response, NextFunction } from "express";
+import routes from "../server/routes.js";
 
-export default createServer();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Add CORS headers for Vercel
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+app.use(routes);
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ message });
+});
+
+export default app;
